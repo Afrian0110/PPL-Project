3758fad4f035535a575b0dc5bbe81217e4f95a30
Afrian0110 <rafiafrian003@gmail.com>
2024-09-22 19:10:31 +0700

    PBKKxPPL

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <h1>Sistem Informasi Pengmbalian Barang Hilang Unib</h1>
+        <p>This is the landing page for our PPL Project.</p>
+    </div>
+</body>
+</html>
